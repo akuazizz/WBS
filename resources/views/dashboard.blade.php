0add6d4c6b95bc1e8d24fdf49e2d3c5adcd2739f
Muhammad Abdul Aziz <muhammadabdulaziz772@gmail.com>
2025-07-02 09:49:15 +0700

    update treshold

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -858,7 +858,7 @@
                     }
                 });
             }, {
-                threshold: 0.5
+                threshold: 0.3
             });
 
             sections.forEach(section => observer.observe(section));
