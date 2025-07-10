7cfdd4aca2e5a9022e84b99039d568604e36b9d3
Muhammad Abdul Aziz <muhammadabdulaziz772@gmail.com>
2025-07-10 11:39:20 +0700

    update warna tuisan

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="flex justify-between items-center mb-6">
-    <h1 class="text-2xl md:text-3xl font-bold text-gray-800">Manajemen User</h1>
+    <h1 class="text-2xl md:text-3xl font-bold text-gray-800" style="color: #004F98;">Manajemen User</h1>
     <a href="{{ route('admin.users.create') }}"
     class="px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-700 transition-colors">
     + Tambah User Baru
