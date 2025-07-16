940e92e0da8dbe69e70218b6e2464862de4ba1f9
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-07-16 14:02:17 +0700

    menambahkan file pdf panduan WBS di page alur pengaduan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -218,15 +218,16 @@
                                 d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
                         </svg>
                         Video Tutorial Panduan WBS
-                    </a>
-                    <a href="/img/Buku_Panduan_WBS_2024_Pelapor.pdf" target="_blank"
-                        class="inline-flex items-center justify-center px-6 py-3 bg-inspektorat-blue border border-gray-300 rounded-lg shadow-sm text-white font-semibold hover:bg-[#C0C0C0] transition-colors">
-                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                        </svg>
-                        Buku Panduan Pelapor
-                    </a>
+                </a>
+                <a href="{{ asset('images/Panduan-WBS.pdf') }}" target="_blank"
+                    class="inline-flex items-center justify-center px-6 py-3 bg-inspektorat-blue border border-gray-300 rounded-lg shadow-sm text-white font-semibold hover:bg-[#C0C0C0] transition-colors">
+                    <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                            d ="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                    </svg>
+                    Buku Panduan Pelapor
+                </a>
+
                 </div>
 
                 <!-- Tab Switcher -->
